b0d977e1b6bf784c421157f16373f07743e78b35
shihhanwang <swang05@mail.bbk.ac.uk>
2018-07-21 13:00:04 +0100

    16: mail sending | use yaml for mailgun credentials

--- mail_sending.py
+++ mail_sending.py
@@ -1,17 +1,20 @@
 import requests
-import config
+import yaml
 
 from mail_message import message
 
+with open("config.yml") as ymlfile:
+    cfg = yaml.load(ymlfile)
+
 def send_email(payload):
 
-    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(config.sandbox)
-    sender = 'student.com <postmaster@{0}>'.format(config.sandbox)
+    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(cfg['Mailgun']['sandbox'])
+    sender = 'student.com <postmaster@{0}>'.format(cfg['Mailgun']['sandbox'])
     recipient = '{0} <{1}>'.format(payload['payee']['name'], payload['payee']['email'])
 
     request = requests.post(
         request_url,
-        auth=('api', config.key),
+        auth=('api', cfg['Mailgun']['api_key']),
         data={
         'from': sender,
         'to': recipient,
